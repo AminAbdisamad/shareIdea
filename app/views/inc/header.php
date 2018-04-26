f8de465a96443eebffd53c8d1014deedb086551b
Amin Abdisamad <theaminux@gmail.com>
2018-04-26 16:08:01 +0300

    main menu added to menu.php file

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -15,22 +15,6 @@
     <title><?php echo SITENAME ?></title>
 </head>
 <body>
-    <!-- Top Menu -->
-  <header>
-        <div class="navbar-fixed">
-            <nav>
-                <div class="nav-wrapper white ">
-                    <div class="container">
-                        <a href="<?php echo URLROOT?>" class="brand-logo "><img src="<?php echo URLROOT ?>/public/images/shareidea_logo.png" class="" style="margin-top:10px;"></a>
-                        <ul class="right hide-on-med-and-down">
-                        <li><a class="blue-text text-darken-3" href="<?php echo URLROOT?>pages/about">About</a></li>
-                        <li><a class="blue-text text-darken-3" href="<?php echo URLROOT?>users/login">Login</a></li>
-                        <li><a class="blue-text text-darken-3" href="<?php echo URLROOT?>users/register">Register</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
-  <!-- Top Menu end--> 
+<!-- Main Menu here--> 
+<?php require_once APPROOT . '/views/inc/menu.php';?> 
     
