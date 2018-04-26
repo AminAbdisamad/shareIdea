153908bfefa84925ee73df98214a95e9b6e602c4
Amin Abdisamad <theaminux@gmail.com>
2018-04-26 04:08:40 +0300

    Added responsive to pages/index

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,52 +2,52 @@
 <?php require_once APPROOT . '/views/inc/header.php';?>
 <main>
     <div class="row " style="margin-bottom:0px;">
-        <div class="col s6 blue darken-3">
+        <div class="col s12 m6 l6 blue darken-3">
             <div class="container">
             <a href="<?php echo URLROOT?>"><img src="<?php echo URLROOT ?>/public/images/shareidea_med_logo.png" class="" style="margin-top:60px; margin-bottom:40px;"></a>
                 <div class="row">
-                    <div class="col s2" >
+                    <div class="col s2 m4 l2" >
                         <i class="material-icons medium white-text">group_add</i>
                     </div>
-                    <div class="col s10">
+                    <div class="col s9 m8 l10">
                         <p class="white-text" style="font-size:16px;" >You need to register before initiating an application</p>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col s2" >
+                    <div class="col s2 m4 l2" >
                         <i class="material-icons medium white-text">person_add</i>
                     </div>
-                    <div class="col s10">
+                    <div class="col s9 m8 l10">
                         <p class="white-text" style="font-size:16px;" >Create Your Account for free
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col s2" >
+                    <div class="col s2 m4 l2" >
                         <i class="material-icons medium white-text">person_pin</i>
                     </div>
-                    <div class="col s10">
+                    <div class="col s9 m8 l10">
                         <p class="white-text" style="font-size:16px;" >Hear business ideas of your friends</p>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col s2" >
+                    <div class="col s2 m4 l2" >
                         <i class="material-icons medium white-text">chat</i>
                     </div>
-                    <div class="col s10">
+                    <div class="col s9 m8 l10">
                         <p class="white-text" style="font-size:16px;" >Join the conversation</p>
                     </div>
                 </div> <!-- end of row -->
     
             </div>
         </div>
-        <div class="col s6 white">
+        <div class="col col s12 m6 l6  white">
             <div class="container">
             <div class="row" style="margin-top:55px;">
                 <div class="row">
-                    <div class="col s4"> 
+                    <div class="col s12 m12 l4"> 
                         <h3 class="flow-text blue-text text-darken-3"> Sign in </h3> 
                     </div>
-                    <div class="col s8 right-align"> <!-- Create Account-->
+                    <div class="col s12 m12 l8 right-align"> <!-- Create Account-->
                         <p style="font-size:16px;">Don't have an account ? 
                         <a href="#">Create now </a>
                         </p>
