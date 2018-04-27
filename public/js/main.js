302f70a9278aad84b90511c25b9d60bda6259dbd
Amin Abdisamad <theaminux@gmail.com>
2018-04-27 17:16:50 +0300

    Flash messages should disappear after 5s added to main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
 /* responsive sidebar menu */
 const elem = document.querySelector('.sidenav');
 M.Sidenav.init(elem, {});
+
+/* Flash messages should disappear after 5s */
+const msg = document.querySelector('#msg-flash');
+setTimeout(() => { msg.remove(); }, 5000);
+
